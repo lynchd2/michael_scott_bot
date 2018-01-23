2b78b5d043ec6d450b4787caebfb97b2698e6f37
lynchd2 <lynchd2@tcnj.edu>
2018-01-22 23:04:29 -0500

    Add more phrases for michael scott

--- pongbot.rb
+++ pongbot.rb
@@ -1,9 +1,32 @@
 require 'slack-ruby-bot'
 
 class PongBot < SlackRubyBot::Bot
-  command 'ping' do |client, data, match|
-    client.say(text: 'pong', channel: data.channel)
+  operator 'that is really big' do |client, data, match|
+    client.say(text: "that's what she said", channel: data.channel)
   end
+  operator 'right on time' do |client, data,match|
+    client.say(test: "that's what she said", channel: data.channel)
+  end
+  operator 'too big' do |client, data, match|
+    client.say(text: "that's what she said", channel: data.channel)
+  end
+  operator 'too much' do |client, data,match|
+    client.say(test: "that's what she said", channel: data.channel)
+  end
+  operator 'i can go now' do |client, data, match|
+    client.say(text: "that's what she said", channel: data.channel)
+  end
+  operator 'right on time' do |client, data,match|
+    client.say(test: "that's what she said", channel: data.channel)
+  end
+  operator 'come on' do |client, data,match|
+    client.say(test: "that's what she said", channel: data.channel)
+  end
+  operator 'hurry up' do |client, data,match|
+    client.say(test: "that's what she said", channel: data.channel)
+  end
+
+
 end
 
 PongBot.run
